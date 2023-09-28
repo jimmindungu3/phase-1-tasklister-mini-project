721bb5d0c5e033b52b8e1a21c8aa21145fa96ed0
jamesndungu <jimmindungu3@gmail.com>
2023-09-28 14:23:35 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const form = document.getElementById("create-task-form");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const taskDescription = document.getElementById("new-task-description").value;
+  
+      const taskList = document.getElementById("tasks");
+      const listItem = document.createElement("li");
+      listItem.textContent = taskDescription;
+      taskList.appendChild(listItem);
+  
+
+      document.getElementById("new-task-description").value = "";
+    });
+  });
+  
